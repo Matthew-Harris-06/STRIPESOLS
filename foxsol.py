d7f73dc677b02f0fdc4499b8c3f61aff123297d6
Matthew Harris <matt.harris1530@gmail.com>
2023-11-19 07:06:48 -0500

    Add files via upload

---
+++ foxsol.py
@@ -0,0 +1,13 @@
+def foxsol(distance):
+    distance = distance
+    steps = 0
+    for x in range(5,0,-1):
+        
+        steps += distance // x
+        distance -= (distance // x)* x
+        
+    return steps   
+                
+                
+            
+print(foxsol(149))
